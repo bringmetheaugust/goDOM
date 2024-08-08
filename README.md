dcefc03c7f578119764f50dd2108ad97333f6032
bringmetheaugust <bringmetheaugust@gmail.com>
2024-08-08 16:53:32 +0300

    add element search methods

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 <p align="center">
     <a href="https://pkg.go.dev/github.com/bringmetheaugust/goDOM"><img src="https://pkg.go.dev/badge/github.com/stretchr/testify" alt="Doc reference"></a>
     <a href="https://lh3.googleusercontent.com/proxy/w2a-pc4X9z2kuDWoXKnSF8pY6ngZvjVuZOAXMz3ZR8NwaUj9a-KsJnpcjtUSRO9QtFV6vMb3YoHWWv6k43Cb6bHOJEka19uE54GWtVx7Lru8gi10I_968eA2thkA0dL1O-zA8WT24cI"><img src="https://img.shields.io/badge/go%20version-1.21.5-61CFDD.svg?style=flat-square" alt="Golang version"></a>
-    <a href="https://cs4.pikabu.ru/post_img/big/2014/12/15/4/1418619408_1209550583.jpg"><img src="https://img.shields.io/badge/version-1.2.3-blue" alt="project version"></a>
+    <a href="https://cs4.pikabu.ru/post_img/big/2014/12/15/4/1418619408_1209550583.jpg"><img src="https://img.shields.io/badge/version-0.1.6-blue" alt="project version"></a>
 </p>
 
 Made by front-ender for front-enders.   
@@ -30,10 +30,11 @@ package motherfckrs
 import "github.com/bringmetheaugust/goDOM"
 
 func main() {
+    bytes := // HTML markup as bytes (from HTTP request, files, etc.)
     document, err := goDom.Create(bytes)       // create document (DOM with API, like in browser)
-    if err != nil {return}                     // if markup is invalid
+    if err != nil {return}                     // check if markup is invalid
     lol, err := document.QuerySelector("#lol") // <a id="lol" class="pipi" href="http://lol.com">
-    if err != nil {return}                     // if element not found
+    if err != nil {return}                     // check if element not found
     fmt.Println(lol.ClassList)                 // ["pipi"]
     fmt.Println(lol.Attributes)                // {"id": "lol", class: "pipi", "href": "http://lol.com"}
     fmt.Println(lol.GetAttribute("href"))      // "http://lol.com"
@@ -42,29 +43,38 @@ func main() {
 
 ## Docs
 
-### DOM API methods
+### Document
 
-* `QuerySelector`
-* `QuerySelectorAll`
-* `GetElementById`
-* `GetElementsByClassName`
-* `GetElementsByTagName`
+ * methods
 
-### DOM element's methods
+    * `QuerySelector` (doesn't support `*`, `>`, `+`, `~`, pseudo-elements, pseudo-classes)
+    * `QuerySelectorAll` (doesn't support `*`, `>`, `+`, `~`, pseudo-elements, pseudo-classes)
+    * `GetElementById`
+    * `GetElementsByClassName`
+    * `GetElementsByTagName`
 
-* `GetAttribute`
-* `HasAttribute`
+### Element
 
-### DOM element's fields
+ * methods
 
-* `TagName`
-* `TextContent`
-* `Attributes`
-* `Children`
-* `ClassName`
-* `ClassList`
-* `Id`
-* `ParentElement`
+    * `GetAttribute`
+    * `HasAttribute`
+    * `QuerySelector` (doesn't support `*`, `>`, `+`, `~`, pseudo-elements, pseudo-classes)
+    * `QuerySelectorAll` (doesn't support `*`, `>`, `+`, `~`, pseudo-elements, pseudo-classes)
+    * `GetElementById`
+    * `GetElementsByClassName`
+    * `GetElementsByTagName`
+
+ * fields
+
+    * `TagName`
+    * `TextContent` element text (ex. `innerHTML` in **DOM**)
+    * `Attributes`
+    * `Children`
+    * `ClassName`
+    * `ClassList`
+    * `Id`
+    * `ParentElement`
 
 ## Development
 
---
+++ domSearchApi.go
@@ -0,0 +1,217 @@
+package goDom
+
+import (
+	"slices"
+
+	"github.com/bringmetheaugust/goDOM/tools"
+)
+
+// DOM API search methods.
+type domSearchAPI struct{}
+
+func (a domSearchAPI) _querySelector(queryStr string, el Element) (Element, error) {
+	q, err := parseQuery(queryStr)
+
+	if err != nil {
+		return Element{}, err
+	}
+
+	return a.findElementByQuery(*q, el)
+}
+
+func (a domSearchAPI) _querySelectorAll(queryStr string, el Element) ([]Element, error) {
+	queries, err := parseQueries(queryStr)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var matches []Element
+
+	for _, q := range queries {
+		res, err := a.findElementsByQuery(q, el)
+
+		if err != nil {
+			continue
+		}
+
+		matches = append(matches, res...)
+	}
+
+	if len(matches) == 0 {
+		return nil, notFoundErr{Params: "query: " + queryStr}
+	}
+
+	return matches, nil
+}
+
+func (a domSearchAPI) _getElementById(id string, el Element) (Element, error) {
+	res, err := a.findByField("Id", id, el)
+
+	if err != nil {
+		return Element{}, err
+	}
+
+	return res[0], nil
+}
+
+func (a domSearchAPI) _getElementsByClassName(class string, el Element) ([]Element, error) {
+	conditionFn := func(el Element) bool {
+		return slices.Contains(el.ClassList, class)
+	}
+
+	return a.findAllByCondition(conditionFn, el)
+}
+
+func (a domSearchAPI) _getElementsByTagName(tag string, el Element) ([]Element, error) {
+	return a.findByField("TagName", tag, el)
+}
+
+// Check if element matches one level query.
+func (a domSearchAPI) elementMatchesQuery(q query) func(Element) bool {
+	conditionFn := func(el Element) bool {
+		if q.tagName != "" && el.TagName != q.tagName {
+			return false
+		}
+
+		if q.id != "" && el.Id != q.id {
+			return false
+		}
+
+		// check if each class from query contains element
+		for _, class := range q.classList {
+			if !slices.Contains(el.ClassList, class) {
+				return false
+			}
+		}
+
+		// check if each attribute from query contains element
+		for k, v := range q.attributes {
+			attr, ok := el.Attributes[k]
+
+			if !ok || (v != "" && v == attr) {
+				return false
+			}
+		}
+
+		return true
+	}
+
+	return conditionFn
+}
+
+// Find one (first) element which matches all levels query.
+func (a domSearchAPI) findElementByQuery(q query, el Element) (Element, error) {
+	conditionFn := a.elementMatchesQuery(q)
+	res, err := a.findOneByCondition(conditionFn, el)
+
+	if err != nil {
+		return Element{}, err
+	}
+
+	if q.child == nil {
+		return res, nil
+	}
+
+	return a.findElementByQuery(*q.child, res)
+}
+
+// Find elements which matches all levels query.
+func (a domSearchAPI) findElementsByQuery(q query, el Element) ([]Element, error) {
+	var matches []Element
+
+	// find elements which match first query level
+	conditionFn := a.elementMatchesQuery(q)
+	res, err := a.findAllByCondition(conditionFn, el)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if q.child == nil {
+		return res, nil
+	}
+
+	for _, match := range res {
+		res, err := a.findElementsByQuery(*q.child, match)
+
+		if err != nil {
+			continue
+		}
+
+		matches = append(matches, res...)
+	}
+
+	return matches, nil
+}
+
+// FInd elements by field. Only for cases when field's value is string.
+func (a domSearchAPI) findByField(field string, val string, el Element) ([]Element, error) {
+	conditionFn := func(el Element) bool {
+		fieldValue, err := tools.GetFieldValue(&el, field)
+
+		if err != nil {
+			return false
+		}
+
+		return fieldValue == val
+	}
+
+	return a.findAllByCondition(conditionFn, el)
+}
+
+// Find elements by conditions.
+func (a domSearchAPI) findAllByCondition(conditionFn func(Element) bool, el Element) ([]Element, error) {
+	var matches []Element
+
+	if conditionFn(el) {
+		matches = append(matches, el)
+	}
+
+	for _, child := range el.Children {
+		res, err := a.findAllByCondition(conditionFn, child)
+
+		if err == nil {
+			matches = append(matches, res...)
+		}
+	}
+
+	if len(matches) == 0 {
+		return nil, notFoundErr{}
+	}
+
+	return matches, nil
+}
+
+// Find first element by conditions.
+func (a domSearchAPI) findOneByCondition(conditionFn func(Element) bool, el Element) (Element, error) {
+	if conditionFn(el) {
+		return el, nil
+	}
+
+	for _, child := range el.Children {
+		res, err := a.findOneByCondition(conditionFn, child)
+
+		if err != nil {
+			continue
+		}
+
+		return res, nil
+	}
+
+	return Element{}, notFoundErr{}
+}
+
+// Find elements by attribute.
+// Temporary deprecated.
+// func (a domSearchAPI) findByAttribute(attr string, val string, el Element) ([]Element, error) {
+// 	conditionFn := func(el Element) bool {
+// 		if v, ok := el.Attributes[attr]; ok && v == val {
+// 			return true
+// 		}
+
+// 		return false
+// 	}
+
+// 	return a.findAllByCondition(conditionFn, el)
+// }
